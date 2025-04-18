e0fe518808baa39c2063e0b6728a6f37b5b4bbf3
GvidoXD600 <117649746+GvidoXD600@users.noreply.github.com>
2025-04-18 18:45:56 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -10,3 +10,14 @@ JOIN
     profile ON player.ID = profile.player_id
 WHERE 
     profile.playtime_hours < 50;
+
+
+-- Query 2 
+-- Show players whose playtime is greater than the average playtime
+SELECT player.Username, SUM(profile.playtime_hours) AS total_playtime
+FROM player
+JOIN profile ON player.ID = profile.player_id
+GROUP BY player.Username
+HAVING total_playtime > (
+  SELECT AVG(playtime_hours) FROM profile
+);
