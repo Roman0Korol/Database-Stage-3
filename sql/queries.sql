191517739985ff81692551a27ec31824247c4960
Roman0Korol <romankorolovich@live.ie>
2025-04-18 18:49:00 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -12,7 +12,7 @@ WHERE
     profile.playtime_hours < 50;
 
 
--- Query 2 
+-- Query 6 
 -- Show players whose playtime is greater than the average playtime
 SELECT player.Username, SUM(profile.playtime_hours) AS total_playtime
 FROM player
@@ -21,3 +21,23 @@ GROUP BY player.Username
 HAVING total_playtime > (
   SELECT AVG(playtime_hours) FROM profile
 );
+
+-- Query 6
+-- Classify players based on their total playtime hours
+SELECT 
+    Player.Username,
+    SUM(Profile.playtime_hours) AS total_playtime,
+    CASE
+        WHEN SUM(Profile.playtime_hours) > 100 THEN 'Hardcore'
+        WHEN SUM(Profile.playtime_hours) > 50 THEN 'Regular'
+        WHEN SUM(Profile.playtime_hours) > 10 THEN 'Casual'
+        ELSE 'Newbie'
+    END AS player_category
+FROM 
+    Player
+JOIN 
+    Profile ON Player.ID = Profile.player_id
+GROUP BY 
+    Player.Username
+ORDER BY 
+    total_playtime DESC;
